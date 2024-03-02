0661fab5ae9eab358629ad9687b8564108f97f50
Abhishek2149 <abhi.jaybhaye2000@gmail.com>
2024-03-02 19:34:52 +0530

    add

---
+++ src/components/Form-1.js
@@ -0,0 +1,68 @@
+import React,{Component} from "react";
+
+class Form extends Component{
+    constructor(){
+        super()
+        this.state = {
+            userName : "",
+            comments : "",
+            topic : "React"
+        }
+    }
+    handleUserName = (event)=>{
+        this.setState({
+            userName:event.target.value
+        })
+    }
+
+    handleComment = (event) =>{
+        this.setState({
+            comments:event.target.value
+        })
+    } 
+
+    handleTopic = (event) =>{
+        this.setState({
+            topic:event.target.value
+        })
+    }
+
+    handleSubmit = (event) =>{
+        alert (`${this.state.topic} ${this.state.userName} ${this.state.comments}`)
+        event.ppreventDefault()
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>UserName</label>
+                    <input
+                        value={this.state.userName}
+                        onChange={this.handleUserName}
+                    />
+                </div>
+
+                <div>
+                    <label>Comment</label>
+                    <textarea
+                        value={this.state.comments}
+                        onChange={this.handleComment}
+                    />
+                </div>
+
+                <div>
+                    <label>Select Topic</label>
+                    <select value={this.state.topic} onChange={this.handleTopic}>
+                        <option value="react">React</option>
+                        <option value="cypress">Cypress</option>
+                        <option value="java">Java</option>
+                        <option value="playwright">PlayWright</option>
+                    </select>
+                </div>
+                <button type="submit">Submit Form</button>
+            </form>
+        )
+    }
+}
+export default Form
